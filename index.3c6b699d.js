da799a0062811d3369829899d71773800602d393
LizaDiego <150027785+LizaDiego@users.noreply.github.com>
2024-02-05 16:24:19 +0000

    Deploying to gh-pages from @ LizaDiego/project-chocnut@666a1e4abfc456fa4872e0830da6ef40c5b21e2d 🚀

---
+++ index.3c6b699d.js
@@ -0,0 +1,2 @@
+document.getElementById("play-button").addEventListener("click",(function(){var e=document.getElementById("video"),t=document.getElementById("play-button");e.style.display="block",t.style.display="none",e.play()}));
+//# sourceMappingURL=index.3c6b699d.js.map
