c5eda767dc755933645916995ad613e820cff400
LizaDiego <150027785+LizaDiego@users.noreply.github.com>
2024-02-05 12:48:06 +0000

    Deploying to gh-pages from @ LizaDiego/project-chocnut@b4813b0e81586318b68631ee7cc8c8ce4ef1ffc1 🚀

---
+++ index.8bd7d887.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open-review]"),closeModalBtn:document.querySelector("[data-modal-close-review]"),modal:document.querySelector("[data-modal-review]")};function o(){e.modal.classList.toggle("is-hidden-review")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.8bd7d887.js.map
---
+++ index.ea7769ba.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open-review]"),closeModalBtn:document.querySelector("[data-modal-close-review]"),modal:document.querySelector("[data-modal-review]")};function o(){e.modal.classList.toggle("is-hidden-review")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.ea7769ba.js.map
