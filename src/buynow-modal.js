4b2ebb666e12b8deb75f4bfe56dea89dcd1e5a98
emersoneyu <amazon@medishure.com>
2024-02-06 16:49:54 +0800

    recode3
    recode3 added modal but not working

--- src/buynow-modal.js
+++ src/buynow-modal.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open-review]'),
-    closeModalBtn: document.querySelector('[data-modal-close-review]'),
-    modal: document.querySelector('[data-modal-review]'),
+    openModalBtn: document.querySelector('[data-modal-open-buynow]'),
+    closeModalBtn: document.querySelector('[data-modal-close-buynow]'),
+    modal: document.querySelector('[data-modal-buynow]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden-review');
+    refs.modal.classList.toggle('is-hidden-buynow');
   }
 })();
