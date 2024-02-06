ba2a77cfbd032efbb8176cc91102b3bffd46645f
LizaDiego <150027785+LizaDiego@users.noreply.github.com>
2024-02-06 15:43:15 +0000

    Deploying to gh-pages from @ LizaDiego/project-chocnut@4ae581856b762e67e1d8553c344c0f78327d6df5 🚀

---
+++ index.fa7251f0.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{loop:!0,slidesPerView:1,spaceBetween:0,breakpoints:{375:{slidesPerView:1,spaceBetween:0},768:{slidesPerView:2.6,spaceBetween:18},1200:{slidesPerView:4,spaceBetween:18}},pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.fa7251f0.js.map
